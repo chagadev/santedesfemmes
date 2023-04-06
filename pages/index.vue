dcef84206929f04df8b34715177bb6f01d1a10f8
Pascal Martineau <info@chaga.dev>
2023-04-06 13:32:37 -0400

    chore: initial nuxt project

---
+++ pages/index.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+// SEO
+useSeoMeta({ title: "Home" });
+useSchemaOrg([defineWebPage()]);
+</script>
+
+<template>
+  <div id="home">
+    <ThePageHeader title="Welcome!" />
+  </div>
+</template>
